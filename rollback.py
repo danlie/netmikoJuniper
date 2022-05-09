2a33a52120df4039ae66ef3352b63e3783463be2
danlie <danlie@g.ucla.edu>
2022-05-08 19:48:08 -0700

    put module into code

--- rollback.py
+++ rollback.py
@@ -1,3 +1,13 @@
+from netmiko import ConnectHandler
+import dispaly
+junipersw88_2 = {
+	'device_type': 'juniper',
+	'host':   '192.168.88.2',
+	'username': 'root',
+	'password': 'juniper123',
+	'port' : 22,
+}
+net_connect = ConnectHandler(**junipersw88_2)
 rollback_res = ["rollback rescue"]
 net_connect.send_config_set(rollback_res, exit_config_mode=False)
 net_connect.commit()
