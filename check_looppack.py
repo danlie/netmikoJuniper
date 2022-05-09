a10d7b6a73835175cf1c268c05aa0fdd84c0573f
danlie <danlie@g.ucla.edu>
2022-05-09 11:57:02 -0700

    for OS

--- check_looppack.py
+++ check_looppack.py
@@ -1,9 +1,9 @@
-from netmiko import ConnectHandler
-import dispaly
-junipersw88_2 = {
-	'device_type': 'juniper',
-	'host':   '192.168.88.2',
-	'username': 'root',
-	'password': 'juniper123',
-	'port' : 22,
-}
+# from netmiko import ConnectHandler
+# import dispaly
+# junipersw88_2 = {
+# 	'device_type': 'juniper',
+# 	'host':   '192.168.88.2',
+# 	'username': 'root',
+# 	'password': 'juniper123',
+# 	'port' : 22,
+# }
